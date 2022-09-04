e431f713710dbe40494f0577ecc5c02e30157809
Todor <t.ilivanov@gmail.com>
2022-09-04 18:32:59 +0300

    add: add TaskManager module

--- lib/job_processor.ex
+++ lib/job_processor.ex
@@ -1,17 +1,21 @@
+alias JobProcessor.TaskManager, as: TaskManager
+
 defmodule JobProcessor do
   @type task :: %{command: String.t(), name: String.t(), requires: list(String.t())}
   @spec process(%{tasks: [task()]}) :: list
   def process(tasks) do
-    create_tasks(tasks)
+    {:ok, manager_pid} = TaskManager.start_link()
+    create_tasks(manager_pid, tasks)
     tasks |> Enum.map(fn task -> %{name: task["name"], command: task["command"]} end)
   end
 
-  def create_tasks([]), do: :ok
+  def create_tasks(_manager_pid, []), do: :ok
 
-  def create_tasks(tasks) do
+  def create_tasks(manager_pid, tasks) do
     [head | tail] = tasks
-    spawn(fn -> do_task(head) end)
-    create_tasks(tail)
+    task_pid = spawn(fn -> do_task(head) end)
+    send(manager_pid, {:put, task_pid, head})
+    create_tasks(manager_pid, tail)
   end
 
   def do_task(task) do
