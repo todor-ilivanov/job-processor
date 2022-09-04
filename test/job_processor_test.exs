88477c1ea28bb26f75bb096ae8dce0f37c2f0b53
Todor <t.ilivanov@gmail.com>
2022-09-04 14:22:42 +0300

    test: add tests for /process endpoint

--- test/job_processor_test.exs
+++ test/job_processor_test.exs
@@ -12,12 +12,72 @@ defmodule JobProcessorTest.Router do
     assert conn.resp_body == "OK"
   end
 
-  test "/process returns back the request" do
-    req_body = %{tasks: [%{command: "touch /tmp/file1", name: "task-1"}]}
+  test "/process returns empty list when no request body is provided" do
+    assert_process_endpoint_successful()
+  end
+
+  test "/process returns multiple correctly processed jobs in order with no dependencies" do
+    req_body = %{
+      "tasks" => [
+        %{"command" => "touch /tmp/file1", "name" => "task-1"},
+        %{"command" => "cat /tmp/file1", "name" => "task-2"},
+        %{"command" => "echo 'Hello World!' > /tmp/file1", "name" => "task-3"}
+      ]
+    }
+
+    expected_resp = [
+      %{"command" => "touch /tmp/file1", "name" => "task-1"},
+      %{"command" => "cat /tmp/file1", "name" => "task-2"},
+      %{"command" => "echo 'Hello World!' > /tmp/file1", "name" => "task-3"}
+    ]
+
+    assert_process_endpoint_successful(req_body, expected_resp)
+  end
+
+  test "/process returns multiple correctly processed jobs with multiple dependencies" do
+    req_body = %{
+      "tasks" => [
+        %{"command" => "touch /tmp/file1", "name" => "task-1"},
+        %{
+          "command" => "cat /tmp/file1",
+          "name" => "task-2",
+          "requires" => ["task-3"]
+        },
+        %{
+          "command" => "echo 'Hello World!' > /tmp/file1",
+          "name" => "task-3",
+          "requires" => ["task-1"]
+        },
+        %{
+          "command" => "rm /tmp/file1",
+          "name" => "task-4",
+          "requires" => ["task-2", "task-3"]
+        }
+      ]
+    }
+
+    expected_resp = [
+      %{"command" => "touch /tmp/file1", "name" => "task-1"},
+      %{"command" => "echo 'Hello World!' > /tmp/file1", "name" => "task-3"},
+      %{"command" => "cat /tmp/file1", "name" => "task-2"},
+      %{"command" => "rm /tmp/file1", "name" => "task-4"}
+    ]
+
+    assert_process_endpoint_successful(req_body, expected_resp)
+  end
+
+  test "/process returns error on malformed request" do
+    conn = conn(:post, "/process", %{dogs: ["small", "medium", "large"]})
+    conn = JobProcessor.Router.call(conn, @opts)
+    assert conn.state == :sent
+    assert conn.status == 400
+  end
+
+  defp assert_process_endpoint_successful(req_body \\ %{}, expected_resp \\ []) do
     conn = conn(:post, "/process", req_body)
     conn = JobProcessor.Router.call(conn, @opts)
     assert conn.state == :sent
     assert conn.status == 200
-    assert conn.resp_body == Jason.encode!(req_body)
+    assert conn.resp_body == Jason.encode!(expected_resp)
   end
 end
